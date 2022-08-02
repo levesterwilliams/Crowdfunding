8574f45b760339b2d42b028451c38bdf369a14cd
Abby Wank <abbywank@Abbys-MacBook-Air-2.local>
2022-08-02 17:29:13 -0400

    api.js file abby

--- admin/api.js
+++ admin/api.js
@@ -359,6 +359,30 @@ app.use('/allOrgs', (req, res) => {
     });
 
 
+/*
+Handle the form submission to update an org's name
+*/
+app.use('/updateOrgName', (req, res) => {
+	var filter = {"_id" : req.query.id };
+
+	var update = { "name" : req.query.name, "description" : req.query.description };
+	
+	var action = { "$set" : update };
+
+	Organization.findOneAndUpdate( filter, action, { new : true }, (err, result) => {
+		if (err) {
+		    res.json({ "status": "error", "data" : err});
+		}
+		else {
+		    //console.log(result);
+		    res.json({ "status": "success", "data" : result});
+		}
+	    });
+	
+    });
+
+
+
 
 /********************************************************/
 
